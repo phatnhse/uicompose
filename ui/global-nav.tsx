fe6ad1418c846d8ef02e0b025d95bb15e58a432b
phat.nguyenhoang <phat.nguyenhoang@lazada.com>
2023-07-18 11:23:30 +0700

    Testing

--- ui/global-nav.tsx
+++ ui/global-nav.tsx
@@ -26,7 +26,7 @@ export function GlobalNav() {
           </div>
 
           <h3 className="font-semibold tracking-wide text-gray-400 group-hover:text-gray-50">
-            App Router
+            Chu Thon`
           </h3>
         </Link>
       </div>
